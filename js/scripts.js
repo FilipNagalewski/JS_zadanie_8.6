6058baf30a647c8632fa4a2867aa2f0d4e3f4553
Filip Nagalewski <nagalewski@gmail.com>
2017-03-27 10:10:24 +0200

    new version

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 function getTriangleArea(a, h) {
   var triangleArea;
-  if (a > 0 && h > 0 && !isNaN(Number(a)) && !isNaN(Number(h))) {
-    triangleArea = a*h/2,
-    alert(triangleArea);
-  } else {
-    alert('Nieprawidłowe dane');
+
+  if (isNaN(Number(a)) || isNaN(Number(h))) {
+    console.log('Nieprawidłowe dane');
+    return;
   }
-  return triangleArea;
+
+  if (a < 0 || h < 0) {
+    console.log('Nieprawidłowe dane');
+    return;
+  }
+
+  if (a > 0 && h > 0) {
+    triangleArea = a*h/2;
+    console.log(triangleArea);
+    return triangleArea;
+  }
 }
 
 getTriangleArea(10,6);
 
 var triangle1Area = getTriangleArea(8, 5);
-var triangle2Area = getTriangleArea(4, f);
+var triangle2Area = getTriangleArea(4, 'f');
 var triangle3Area = getTriangleArea(-2, 3);
